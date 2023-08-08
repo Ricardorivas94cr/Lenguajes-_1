3722898c92fbebafee15b790b9d994fd7e75c400
Ricardo Rivas Rocha <133481747+Ricardorivas94cr@users.noreply.github.com>
2023-08-08 00:14:02 -0600

    Programas
    Aquí les muestro lo que realice en esta materia de lenguajes de programmatic 1 la verdad muy interesante en c++

---
+++ Act 1/Edad.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+using namespace std;
+int main ()
+{
+int edad;
+cout<<"ingresa la edad"<<endl;
+cin>>edad;
+if (edad>=18)
+cout<<"Es Mayor de edad"<<endl;
+else 
+cout<<"Es Menor de edad"<<endl;
+
+return 0;	
+	
+}
---
+++ Act 2/Operaciones Basicas en c++.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+double numero1, numero2,suma,resta,multiplicacion,division;
+cout<<"El Valor del Primer Numero es: ";
+cin>>numero1;
+cout<<"El Valor del Segundo Numero es: ";
+cin>>numero2;
+suma=numero1+numero2;
+cout<<"\n La suma es: " <<suma;
+resta=numero1-numero2;
+cout<<"\n La resta es: "<<resta;
+multiplicacion=numero1*numero2;
+cout<<"\n La multiplicacion es: " <<multiplicacion;
+division=numero1/numero2;
+cout<<"\n La Division es: " <<division;
+	
+	
+	
+	
+	return 0;
+}
